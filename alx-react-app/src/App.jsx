db29ee1836e10c5bb52914fb9b856c2d0ed7c5c8
KabeloMohlabeng03 <mohlabengmahlognolo@gmail.com>
2024-08-17 18:23:34 +0200

    Add components to src

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,9 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>
-            <WelcomeMessage />
-        </div>
     </>
   )
 }
