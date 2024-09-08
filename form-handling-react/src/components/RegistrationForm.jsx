4d140f051cd80a29d146aefbf1da0fef84ccf160
KabeloMohlabeng03 <mohlabengmahlognolo@gmail.com>
2024-09-08 18:18:24 +0200

    Fix the problems

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
 const RegistrationForm = () => {
+
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
 
-  const [error, setError] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,32 +20,49 @@ const RegistrationForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const { username, email, password } = formData;
+    const newErrors = {};
 
-    if (!formData.username || !formData.email || !formData.password) {
-      setError("All fields are required.");
+
+    if (!username) {
+      newErrors.username = "Username is required";
+    }
+
+    if (!email) {
+      newErrors.email = "Email is required";
+    }
+
+    if (!password) {
+      newErrors.password = "Password is required";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
     } else {
-      setError("");
+      
+      setErrors({});
       console.log("Form Submitted", formData);
     }
   };
-
 
   const { username, email, password } = formData;
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Registration Form (Controlled)</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+
       <div>
         <label htmlFor="username">Username:</label>
         <input
           type="text"
           id="username"
           name="username"
-          value={username} 
+          value={username}
           onChange={handleChange}
         />
+        {errors.username && <p style={{ color: "red" }}>{errors.username}</p>}
       </div>
+
       <div>
         <label htmlFor="email">Email:</label>
         <input
@@ -54,7 +72,9 @@ const RegistrationForm = () => {
           value={email}
           onChange={handleChange}
         />
+        {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
       </div>
+
       <div>
         <label htmlFor="password">Password:</label>
         <input
@@ -64,7 +84,9 @@ const RegistrationForm = () => {
           value={password}
           onChange={handleChange}
         />
+        {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
       </div>
+
       <button type="submit">Register</button>
     </form>
   );
