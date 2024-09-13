f744aa110dd3c4928f75ac32d5e9beade677cefa
KabeloMohlabeng03 <mohlabengmahlognolo@gmail.com>
2024-09-13 14:04:47 +0200

    Fix the problem I made for the second time

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto my-10 md:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto my-10 md:my-20 rounded-lg shadow-lg">
       <img
         className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
         src="https://via.placeholder.com/150"
@@ -10,8 +10,7 @@ function UserProfile() {
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600">
-        Developer at Example Co. Loves to write code and explore new
-        technologies.
+        Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
